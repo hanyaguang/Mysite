ee7c89388df6df1e6f3ee9cf4038069015926759
hanyaguang <hanyaguang@gmail.com>
2017-07-28 16:26:18 +0800

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
